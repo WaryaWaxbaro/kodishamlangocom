1c0df2fa208c8bbe601137241de793edc3b26ae8
Shakur <shakur.hassan@autolle.com>
2022-06-21 14:40:18 +0300

    add progress bar

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,17 @@
 import { useEffect } from "react";
 import "react-toastify/dist/ReactToastify.css";
+import Router from "next/router";
+import NProgress from "nprogress"; //nprogress module
+import "nprogress/nprogress.css"; //styles of nprogress
+
 import MainLayout from "../layout/MainLayout";
 import UserProvider from "../context/userContext";
 import "../scss/main.scss";
+
+//Binding events.
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
