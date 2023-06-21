87e39ef17adcd12319893c65fd73cbed9f8f0363
Shakur <shakur.hassan@autolle.com>
2023-06-21 23:59:05 +0300

    add back all settings

--- pages/index.js
+++ pages/index.js
@@ -8,10 +8,55 @@ import WhyChooseUs from "../components/WhyChooseUs";
 import Image from "next/image";
 
 export default function Home({ listings }) {
+  const [thumbnails, setThumbnails] = useState([]);
+
+  useEffect(() => {
+    console.log("listings", listings);
+    let parsedListings = JSON.parse(listings) || [];
+
+    const getThumbnails = async (listingIds) => {
+      const thumbnails = listingIds.forEach(async (listingId) => {
+        const thumbanil = await new StorageUploads(
+          `apartments/thumbnails/${listingId}`
+        ).downloadURL();
+        setThumbnails((prevThumbnails) => [
+          ...prevThumbnails,
+          { id: listingId, img: thumbanil },
+        ]);
+        return thumbanil;
+      });
+    };
+    if (parsedListings) {
+      const listingIds = parsedListings.map((listing) => listing.mId);
+      getThumbnails(listingIds);
+    }
+  }, [listings]);
+
   return (
-    <div>
-      <h1>Hello!</h1>
-    </div>
+    <>
+      <Hero />
+      <div className="container d-flex justify-content-center">
+        <div className="d-inline-block mx-auto my-4">
+          <a
+            href="https://tp.media/click?shmarker=373506&promo_id=7786&source_type=banner&type=click&campaign_id=200&trs=179005"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Image
+              src={`https://c200.travelpayouts.com/content?promo_id=7786&shmarker=373506&type=init&trs=179005`}
+              width={728}
+              height={90}
+            />
+          </a>
+        </div>
+      </div>
+      <PopularPlaces />
+      <FeaturedProperties
+        thumbnails={thumbnails}
+        listings={JSON.parse(listings)}
+      />
+      <WhyChooseUs />
+    </>
   );
 }
 
