e33d2aa40cf2635c410054547ab707ed3695afcf
Shakur <shakur.hassan@autolle.com>
2023-06-21 23:12:55 +0300

    remove ad from main page

--- pages/index.js
+++ pages/index.js
@@ -34,21 +34,6 @@ export default function Home({ listings }) {
   return (
     <>
       <Hero />
-      <div className="container d-flex justify-content-center">
-        <div className="d-inline-block mx-auto my-4">
-          <a
-            href="https://tp.media/click?shmarker=373506&promo_id=7786&source_type=banner&type=click&campaign_id=200&trs=179005"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <Image
-              src={`https://c200.travelpayouts.com/content?promo_id=7786&shmarker=373506&type=init&trs=179005`}
-              width={728}
-              height={90}
-            />
-          </a>
-        </div>
-      </div>
       <PopularPlaces />
       <FeaturedProperties
         thumbnails={thumbnails}
